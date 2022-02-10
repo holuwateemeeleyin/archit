f64d3f515975651645c7b5bc3f197b5df0b25def
unknown <abegundeosamuel@gmail.com>
2022-02-10 18:05:58 +0100

    passed sidebar functions to HeaderMobile

--- src/hoc/layout.js
+++ src/hoc/layout.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react';
 import HeaderMobile from '../container/Header/HeaderMobile'
 import Header from '../container/Header/Header'
+import { SideNav } from 'react-simple-sidenav';
 
-const Layout = (props)=> {
-    return (
+export default class layout extends Component {
+    state={
+        showNav:false
+    }
+    ontoggleNav =(action)=> {
+        this.setState({showNav:action})
+    }
+  render() {
+    return <div>
+        <Header/>
+        <HeaderMobile
+            showNav={this.state.showNav}
+            onOpenNav={()=>this.ontoggleNav(true)}
+            onHideNav={()=>this.ontoggleNav(false)}
+        />
         <div>
-            <Header/>
-            <HeaderMobile/>
-            <div>
-                {props.children}
-            </div>
+            {this.props.children}
         </div>
-    )
+    </div>;
+  }
 }
-
-export default Layout
